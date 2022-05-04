53b14f437d9475b31aadec439279b1946a67803f
Saidutt Nagumanthri <saidutt49@gmail.com>
2022-05-04 20:11:17 +0530

    changed add method with 1 parameter instead of 2

--- Add.js
+++ Add.js
@@ -1,4 +1,9 @@
-function Add(a, b) {
-    return a + b;
+function Add(numbers) {
+    let ans = 0;
+    [...numbers].map(n=>{
+        if(n == null) return 0
+        if(!isNaN(n))ans += parseInt(n)
+    })
+    return ans;
 }
 module.exports = Add
--- Add.test.js
+++ Add.test.js
@@ -1,5 +1,11 @@
 const Add = require('./Add')
 
-test('test to add 2 valid number',()=>{
-    expect(Add(1,2)).toBe(3)
+test('test for empty',()=>{
+    expect(Add('')).toBe(0)
+})
+test('test for one number',()=>{
+    expect(Add('1')).toBe(1)
+})
+test('test the 2 numbers',()=>{
+    expect(Add('1,2')).toBe(3)
 })
