46169bcd08cc4129c6a61863452b4248ee7d395c
Saidutt Nagumanthri <saidutt49@gmail.com>
2022-05-08 11:22:03 +0530

    Added functionality to handle custom separator

--- Add.js
+++ Add.js
@@ -1,15 +1,35 @@
 function Add(numbers) { 
-    let ans = 0,negativeError =[];
-    let n = numbers.split(/,|\n/g);
+    let ans = 0,negativeError =[],n = null;
+
+    // checking whether custom delimeter is present in given string or not
+    // if custom delimeter is present extracting custom delimeter from string and removing custom part from the string
+    // if custom delimeter is not present, separating the given string with comma/new-line separators 
+    if(/^\/\//.test(numbers))
+    {
+        let delimeter = numbers.charAt(2)
+        numbers = numbers.substr(numbers.indexOf('\n')+1)
+        n = numbers.split(delimeter)
+    }
+    else{
+        n = numbers.split(/,|\n/g);
+    }
+    
+    // checking for negative numbers in code, if present pushing the negative numbers in an array
     n.some((n)=>{
         if(n<0){
             negativeError.push(n)
         }
-    })   
-    if(negativeError.length > 0) return 'negatives not allowed '+[...negativeError]
+    })
+
+    // throwing error if negative inputs present
+    if(negativeError.length > 0) throw new Error('negatives not allowed, the passed negative numbers are : '+[...negativeError])
+
+
+    // calcuating sum after separating the input string with comma/new-line/custom separators
     for(let i = 0; i < n.length; i++){
         ans += parseInt(n[i] || 0)
     }
     return ans;
 }
+
 module.exports = Add
--- Add.test.js
+++ Add.test.js
@@ -8,12 +8,16 @@ test('test for one number',()=>{
 })
 test('test the numbers with comma separator ',()=>{
     expect(Add('1,2')).toBe(3)
+    expect(Add('1,2,3,4,5,6')).toBe(21)
 })
 test('test the numbers with new line separator  ',()=>{
     expect(Add('1\n2\n3')).toBe(6)
     expect(Add('1\n2\n3\n4')).toBe(10)
 })
 test('test with negative numbers, it should throw error if input has any negative numbers',()=>{
-    expect(Add('1,2,-3')).toBe("negatives not allowed -3");
-    expect(Add('-1,-2,-3')).toBe("negatives not allowed -1,-2,-3");
+    expect(()=>Add('1,2,-3')).toThrowError("negatives not allowed, the passed negative numbers are : -3");
+    expect(()=>Add('-1,-2,-3')).toThrowError("negatives not allowed, the passed negative numbers are : -1,-2,-3");
+})
+test('test with custom delimeter',()=>{
+    expect(Add('//:\n1:2:3:4:5')).toBe(15)
 })
